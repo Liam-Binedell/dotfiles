03c6630abafe9342d0d09470674140b9a37edf79
Liam-Binedell <liam.binedell@gmail.com>
2026-08-13 01:59:56 +0200

    new blink binds

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -7,6 +7,10 @@ return {
           selection = { preselect = false, auto_insert = true },
         },
       },
+      keymap = {
+        ["<Tab>"] = { "select_and_accept", "fallback" },
+        ["S-<Tab>"] = { "select_prev", "fallback" },
+      },
     },
   },
 }
